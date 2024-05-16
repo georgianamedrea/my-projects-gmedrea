9d9c85e3f1d5497ea6d92d53ec0fa2ac06c2e0e3
georgianamedrea <georgianamedrea83@gmail.com>
2024-05-16 21:11:26 +0300

    updates for several posts on feed, friends feeds and groups

--- Social_media_app_JS/js/feed.js
+++ Social_media_app_JS/js/feed.js
@@ -15,14 +15,9 @@ logoutButton.addEventListener('click', () => {
     window.open('../login.html', '_self')
 })
 
-const noOfLikesElem = document.getElementById('likesNumber')
-const noOfSharesElem = document.getElementById('sharesNumber')
-
-const likeButton = document.getElementById('likeButton')
-const shareButton = document.getElementById('shareButton')
-
-let isLiked = false
-let isShared = false
+const postElements = document.getElementsByClassName('post');
+const noOfLikesElem = document.querySelectorAll('.likesNumber');
+const noOfSharesElem = document.querySelectorAll('.sharesNumber');
 
 function getNumberOfLikes() {
     noOfLikesElem.innerText = noOfLikesElem.innerText || 21
@@ -39,94 +34,129 @@ function getData() {
 
 getData()
 
-likeButton.addEventListener('click', function () {
-    isLiked = !isLiked
-    console.dir(noOfLikesElem)
+for (let i = 0; i < postElements.length; i++) {
 
-    noOfLikesElem.innerText = isLiked
-        ? noOfLikesElem.innerText = Number(noOfLikesElem.innerText) + 1
-        : noOfLikesElem.innerText = Number(noOfLikesElem.innerText) - 1
+    const postElement = postElements[i];
 
-    this.classList.toggle('touched')
-})
+    const likeButtons = postElement.querySelectorAll('#likeButton');
+    const shareButtons = postElement.querySelectorAll('#shareButton');
 
-shareButton.addEventListener('click', function () {
-    isShared = !isShared
-    noOfSharesElem.innerText = isShared
-        ? Number(noOfSharesElem.innerText) + 1
-        : Number(noOfSharesElem.innerText) - 1
+    const noOfLikesElems = postElement.querySelectorAll('.likesNumber');
+    const noOfSharesElems = postElement.querySelectorAll('.sharesNumber');
 
-    this.classList.toggle('touched')
-})
 
-const commentButton = document.getElementById('commentButton')
-const commentInput = document.getElementById('commentInput')
-const commentInputButton = document.getElementById('commentInputButton')
+    likeButtons.forEach(likeButton => {
 
-const commentMessage = document.getElementById('commentMessage')
-const removeCommentButton = document.getElementById('removeCommentButton')
+        let isLiked = false
 
-commentMessage.innerText = localStorage.getItem('comment') || commentMessage.innerText;
+        likeButton.addEventListener('click', function (event) {
 
-commentButton.addEventListener('click', function () {
-    commentInput.focus()
-})
+            isLiked = !isLiked
 
-function setComment() {
-    commentMessage.innerText = commentInput.value;
-    localStorage.setItem('comment', JSON.stringify(commentInput.value))
-    commentInput.value = ''
+            noOfLikesElems.forEach(noOfLikesElem => {
+                noOfLikesElem.innerText = isLiked
+                    ? Number(noOfLikesElem.innerText) + 1
+                    : Number(noOfLikesElem.innerText) - 1
+            })
+
+            likeButton.classList.toggle('touched')
+        })
+    })
+
+    shareButtons.forEach(shareButton => {
+
+        let isShared = false
+
+        shareButton.addEventListener('click', function (event) {
+
+            isShared = !isShared
+
+            noOfSharesElems.forEach(noOfSharesElem => {
+                noOfSharesElem.innerText = isShared
+                    ? Number(noOfSharesElem.innerText) + 1
+                    : Number(noOfSharesElem.innerText) - 1
+            })
+
+            shareButton.classList.toggle('touched')
+        })
+    })
 }
 
-commentInput.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-        setComment()
-        this.blur()
+const commentButtons = document.querySelectorAll('#commentButton')
+const newCommentFields = document.querySelectorAll('.newCommentField')
+const insertCommentButtons = document.querySelectorAll('.insertCommentButton')
+
+const commentMessages = document.querySelectorAll('.commentMessage')
+
+for (let i = 0; i < commentButtons.length; i++) {
+
+    let commentButton = commentButtons[i];
+    let newCommentField = newCommentFields[i];
+    let insertCommentButton = insertCommentButtons[i];
+    let commentMessage = commentMessages[i];
+
+
+    commentButton.addEventListener('click', function () {
+        newCommentField.focus()
+    })
+
+    function setComment() {
+        commentMessage.innerText = newCommentField.value;
+        newCommentField.value = '';
     }
-})
 
-commentInputButton.addEventListener('click', setComment)
+    newCommentField.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            setComment()
+            this.blur()
+        }
+    })
 
-const commentText = document.getElementsByClassName('userCommentText')[0]
-commentText.addEventListener('mouseover', function () {
-    removeCommentButton.style.display = 'inline-block'
-})
+    insertCommentButton.addEventListener('click', setComment)
+}
 
-commentText.addEventListener('mouseout', function () {
-    removeCommentButton.style.display = 'none'
-})
+const infoIcons = document.getElementsByClassName('infoIcon')
+const infoMessages = document.getElementsByClassName('infoMessage')
 
-const infoIcon = document.getElementsByClassName('infoIcon')[0]
-const infoMessage = document.getElementsByClassName('infoMessage')[0]
+for (let i = 0; i < infoIcons.length; i++) {
 
-infoIcon.addEventListener('click', function () {
-    if (infoMessage.style.display === 'block') {
+    let infoIcon = infoIcons[i];
+    let infoMessage = infoMessages[i];
+
+    infoIcon.addEventListener('click', function () {
+        if (infoMessage.style.display === 'block') {
+            infoMessage.style.display = 'none'
+        } else {
+            infoMessage.style.display = 'block'
+        }
+    })
+    infoIcon.addEventListener('blur', function () {
         infoMessage.style.display = 'none'
-    } else {
-        infoMessage.style.display = 'block'
-    }
-})
+    })
+}
 
-infoIcon.addEventListener('blur', function () {
-    infoMessage.style.display = 'none'
-})
+const profileOptionsButtons = document.getElementsByClassName('profileOptions')
+const profileOptionsDropdowns = document.getElementsByClassName('profileOptionsDropdown')
 
-const profileOptionsButton = document.getElementsByClassName('profileOptions')[0]
-const profileOptionsDropdown = document.getElementsByClassName('profileOptionsDropdown')[0]
+for (let i = 0; i < postElements.length; i++) {
 
-profileOptionsButton.addEventListener('click', function () {
-    if (profileOptionsDropdown.style.display === 'none'
-        || profileOptionsDropdown.style.display === '') {
-        profileOptionsDropdown.style.display = 'flex'
-    } else {
+    let profileOptionsButton = profileOptionsButtons[i];
+    let profileOptionsDropdown = profileOptionsDropdowns [i];
+
+    profileOptionsButton.addEventListener('click', function () {
+        if (profileOptionsDropdown.style.display === 'none'
+            || profileOptionsDropdown.style.display === '') {
+            profileOptionsDropdown.style.display = 'flex'
+        } else {
+            profileOptionsDropdown.style.display = 'none'
+        }
+
+    })
+
+    profileOptionsButton.addEventListener('blur', function () {
         profileOptionsDropdown.style.display = 'none'
-    }
-
-})
-
-profileOptionsButton.addEventListener('blur', function () {
-    profileOptionsDropdown.style.display = 'none'
-})
+    })
+}
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
@@ -152,8 +182,8 @@ onlineUserElements.forEach(element => {
 
 function sendMessage() {
     document.querySelector('.message').style.display = "none";
-    document.querySelector('.btn').innerText = "Sent!";
-    document.querySelector('.btn').style.backgroundColor = "aquamarine";
+    document.querySelector('.btnSubmit').innerText = "Sent!";
+    document.querySelector('.btnSubmit').style.backgroundColor = "aquamarine";
 }
 
 closeForm()
--- Social_media_app_JS/js/leftNavFeeds.js
+++ Social_media_app_JS/js/leftNavFeeds.js
@@ -1,11 +1,23 @@
-const noOfLikesElement = document.getElementById('likesNumber')
-const noOfSharesElement = document.getElementById('sharesNumber')
+const accountSettingsButton = document.getElementById('accountSettingsButton')
+const accountSettingsList = document.getElementsByClassName('accountSettingsMenu')[0]
 
-const likeButton = document.getElementById('likeButton')
-const shareButton = document.getElementById('shareButton')
+const logoutButton = document.getElementById('logoutButton')
 
-let isLiked = false
-let isShared = false
+accountSettingsButton.addEventListener('click', function () {
+    if (accountSettingsList.style.display === 'flex') {
+        accountSettingsList.style.display = 'none'
+    } else {
+        accountSettingsList.style.display = 'flex'
+    }
+})
+
+logoutButton.addEventListener('click', () => {
+    window.open('../login.html', '_self')
+})
+
+const postElements = document.getElementsByClassName('post');
+const noOfLikesElement = document.querySelectorAll('likesNumber')
+const noOfSharesElement = document.querySelectorAll('sharesNumber')
 
 function getNumberOfLikes() {
     noOfLikesElement.innerText = noOfLikesElement.innerText || 2
@@ -22,92 +34,121 @@ function getData() {
 
 getData()
 
-likeButton.addEventListener('click', function () {
-    isLiked = !isLiked
+for (let i = 0; i < postElements.length; i++) {
 
-    if (isLiked) {
-        noOfLikesElement.innerText = Number(noOfLikesElement.innerText) + 1
-    } else {
-        noOfLikesElement.innerText = Number(noOfLikesElement.innerText) - 1
-    }
+    const postElement = postElements[i];
 
-    this.classList.toggle('touched')
-})
+    const likeButtons = postElement.querySelectorAll('#likeButton');
+    const shareButtons = postElement.querySelectorAll('#shareButton');
+
+    const noOfLikesElements = postElement.querySelectorAll('.likesNumber');
+    const noOfSharesElements = postElement.querySelectorAll('.sharesNumber');
+
+    likeButtons.forEach(likeButton => {
+
+        let isLiked = false
+
+        likeButton.addEventListener('click', function (event) {
+
+            isLiked = !isLiked
+
+            noOfLikesElements.forEach(noOfLikesElement => {
+                noOfLikesElement.innerText = isLiked
+                    ? Number(noOfLikesElement.innerText) + 1
+                    : Number(noOfLikesElement.innerText) - 1
+            })
+
+            likeButton.classList.toggle('touched')
+        })
+
+    })
+
+    shareButtons.forEach(shareButton => {
+
+        let isShared = false
+
+        shareButton.addEventListener('click', function (event) {
+
+            isShared = !isShared
 
 
-shareButton.addEventListener('click', function () {
-    getNumberOfShares()
+            noOfSharesElements.forEach(noOfSharesElement => {
+                noOfSharesElement.innerText = isShared
+                    ? Number(noOfSharesElement.innerText) + 1
+                    : Number(noOfSharesElement.innerText) - 1
+            })
 
-    isShared = !isShared
+            shareButton.classList.toggle('touched')
+        })
 
-    if (isShared) {
-        noOfSharesElement.innerText = Number(noOfSharesElement.innerText) + 1
-    } else {
-        noOfSharesElement.innerText = Number(noOfSharesElement.innerText) - 1
-    }
-
-    this.classList.toggle('touched')
-})
-
-const commentButton = document.getElementById('commentButton')
-const commentInput = document.getElementById('commentInput')
-const commentInputButton = document.getElementById('commentInputButton')
-
-const commentMessage = document.getElementById('commentMessage')
-const removeCommentButton = document.getElementById('removeCommentButton')
-
-commentButton.addEventListener('click', function () {
-    commentInput.focus()
-})
-
-function setComment() {
-    commentMessage.innerText = commentInput.value;
-    commentInput.value = ''
+    })
 }
 
-commentInput.addEventListener('keydown', function (event) {
-    if (event.key === 'Enter') {
-        setComment()
-        this.blur()
+const commentButtons = document.querySelectorAll('#commentButton')
+const newCommentFields = document.querySelectorAll('.newCommentField')
+const insertCommentButtons = document.querySelectorAll('.insertCommentButton')
+
+const commentMessages = document.querySelectorAll('.commentMessage')
+
+for (let i = 0; i < commentButtons.length; i++) {
+
+    let commentButton = commentButtons[i];
+    let newCommentField = newCommentFields[i];
+    let insertCommentButton = insertCommentButtons[i];
+    let commentMessage = commentMessages[i];
+
+
+    commentButton.addEventListener('click', function () {
+        newCommentField.focus()
+    })
+
+    function setComment() {
+        commentMessage.innerText = newCommentField.value;
+        newCommentField.value = '';
     }
-})
 
-commentInputButton.addEventListener('click', setComment)
+    newCommentField.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            setComment()
+            this.blur()
+        }
+    })
 
-const commentText = document.getElementsByClassName('userCommentText')[0]
-commentText.addEventListener('mouseover', function () {
-    removeCommentButton.style.display = 'inline-block'
-})
+    insertCommentButton.addEventListener('click', setComment)
+}
 
-commentText.addEventListener('mouseout', function () {
-    removeCommentButton.style.display = 'none'
-})
+const profileOptionsButtons = document.getElementsByClassName('profileOptions')
+const profileOptionsDropdowns = document.getElementsByClassName('profileOptionsDropdown')
 
-const profileOptionsButton = document.getElementsByClassName('profileOptions')[0]
-const profileOptionsDropdown = document.getElementsByClassName('profileOptionsDropdown')[0]
+for (let i = 0; i < postElements.length; i++) {
 
-profileOptionsButton.addEventListener('click', function () {
-    if (profileOptionsDropdown.style.display === 'none'
-        || profileOptionsDropdown.style.display === '') {
-        profileOptionsDropdown.style.display = 'flex'
-    } else {
-        profileOptionsDropdown.style.display = 'none'
-    }
-})
+    let profileOptionsButton = profileOptionsButtons[i];
+    let profileOptionsDropdown = profileOptionsDropdowns[i];
 
-// profileOptionsButton.addEventListener('blur', function() {
-//     profileOptionsDropdown.style.display = 'none'
-// })
+    profileOptionsButton.addEventListener('click', function () {
+        if (profileOptionsDropdown.style.display === 'none'
+            || profileOptionsDropdown.style.display === '') {
+            profileOptionsDropdown.style.display = 'flex'
+        } else {
+            profileOptionsDropdown.style.display = 'none'
+        }
 
-const hidePostButton = document.getElementById('hidePost')
-const reportPostButton = document.getElementById('reportPost')
-const postElement = document.getElementsByClassName('post')[0]
+    })
 
-hidePostButton.addEventListener('click', function () {
-    postElement.style.display = 'none'
-})
+    const hidePostButtons = document.querySelectorAll('#hidePost')
+    const reportPostButtons = document.querySelectorAll('#reportPost')
 
-reportPostButton.addEventListener('click', function () {
-    alert('After we will review the content, we will let you know whether it did or did not go against our Community Standards.')
-    profileOptionsDropdown.style.display = 'none'
-})
+    hidePostButtons.forEach(hidePostButton => {
+        hidePostButton.addEventListener('click', function () {
+            let post = hidePostButton.closest('.post')
+            post.style.display = 'none'
+        })
+    })
+
+    reportPostButtons.forEach(reportPostButton => {
+        reportPostButton.addEventListener('click', function () {
+            alert('After we will review the content, we will let you know whether it did or did not go against our Community Standards.')
+            profileOptionsDropdown.style.display = 'none'
+        })
+    })
+}
